a20ef8de9cda149c933f0a90152f2b17eaedf7fd
Frankly <franklyadapa@gmail.com>
2024-08-02 17:31:22 -0400

    modified

--- js/app.js
+++ js/app.js
@@ -2,12 +2,6 @@ const menu = document.querySelector("#menu-icon");
 const menuLinks = document.querySelector(".navlist");
 
 menu.addEventListener("click", () => {
-    menu.classList.toggle("close");
-    menuLinks.classList.toggle("open");
+  menu.classList.toggle("close");
+  menuLinks.classList.toggle("open");
 });
-
-// ______ fake
-// menu.onclick = () => {
-//     menu.classList.toggle("close");
-//     menuLinks.classList.toggle("open");
-// };
