8911f681b897dfa0a75a7cb6b26b44b0ac456f9c
Frankly <franklyadapa@gmail.com>
2024-08-14 15:37:20 -0400

    refactor

--- js/app.js
+++ js/app.js
@@ -5,3 +5,14 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("close");
   menuLinks.classList.toggle("open");
 });
+
+const bgImg = document.querySelector(".bg-img");
+window.addEventListener("scroll", (e) => {
+  let sizeScroll = window.scrollY;
+
+  if (sizeScroll >= 1150) {
+    bgImg.classList.add("bghidden");
+  } else {
+    bgImg.classList.remove("bghidden");
+  }
+});
