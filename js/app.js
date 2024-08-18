c92013b771679ea2abcf9ad9ad5d8cfc2eb59f90
Frankly <franklyadapa@gmail.com>
2024-08-17 20:33:40 -0400

    js

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@ menu.addEventListener("click", () => {
 const bgImg = document.querySelector(".bg-img");
 window.addEventListener("scroll", (e) => {
   let sizeScroll = window.scrollY;
-
   if (sizeScroll >= 1150) {
     bgImg.classList.add("bghidden");
   } else {
